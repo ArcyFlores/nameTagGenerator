6de6db7d7212b8003a861ebc26a4e4f3beb00c5e
Arcy Flores <aflores@ccs.neu.edu>
2020-09-03 13:45:14 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import NameTagList from './NameTagList.js';
+import UserInput from './UserInput.js';
+
+class App extends Component {
+  state = {
+    names: [],
+  };
+  addName = name => {
+    var newNames = [name, ...this.state.names];
+    this.setState({ names: newNames });
+  };
+  removeName = clickedIndex => {
+    var filterCallback = (_, index) => index !== clickedIndex;
+    var newNames = this.state.names.filter(filterCallback);
+    this.setState({ names: newNames });
+  };
+  componentDidMount() {
+    var savedNamesString = localStorage.getItem('savedNames');
+    if (savedNamesString) {
+      var savedNames = JSON.parse(savedNamesString);
+      this.setState({ names: savedNames });
+    }
+  }
+  componentDidUpdate() {
+    var savedNamesString = JSON.stringify(this.state.names);
+    localStorage.setItem('savedNames', savedNamesString);
+  }
+  render() {
+    return (
+      <div className='App'>
+        <h1>Name Tag Generator</h1>
+        <UserInput addName={this.addName} />
+        <NameTagList names={this.state.names} removeName={this.removeName} />
+      </div>
+    );
+  }
+}
+
+export default App;
